834117d7658f4d7efd6f08df452d7cd7435b6c85
ShraddhanandKhot <shraddhanand2836@gmail.com>
2026-01-28 15:34:53 +0530

    getSubdomain file updated cleanHost logic changed

--- lib/getSubdomain.tsx
+++ lib/getSubdomain.tsx
@@ -1,4 +1,4 @@
-import { headers } from "next/headers"
+/*import { headers } from "next/headers"
 
 export async function getSubdomain() {
     const headersList = await headers()
@@ -11,5 +11,27 @@ export async function getSubdomain() {
     // localhost OR www → no store
     if (parts.length < 2) return null
 
+    return parts[0]
+} */
+
+import { headers } from "next/headers"
+
+export async function getSubdomain() {
+    const headersList = await headers()
+    const host = headersList.get("host") ?? ""
+
+    const cleanHost = host.split(":")[0] // remove port
+    const parts = cleanHost.split(".")
+
+    // Localhost
+    if (cleanHost === "localhost") return null
+
+    // Root domain (sprynt.works)
+    if (parts.length === 2) return null
+
+    // www.sprynt.works
+    if (parts[0] === "www") return null
+
+    // subdomain.sprynt.works
     return parts[0]
 }
