b3f74a732a04944b36bd96ea171937d9898e241c
ShraddhanandKhot <shraddhanand2836@gmail.com>
2026-02-05 19:22:07 +0530

    icon and web tirle is updated

--- lib/getSubdomain.tsx
+++ lib/getSubdomain.tsx
@@ -1,18 +1,4 @@
-/*import { headers } from "next/headers"
 
-export async function getSubdomain() {
-    const headersList = await headers()
-    const host = headersList.get("host") ?? ""
-
-    const cleanHost = host.split(":")[0]
-
-    const parts = cleanHost.split(".")
-
-    // localhost OR www → no store
-    if (parts.length < 2) return null
-
-    return parts[0]
-} */
 
 import { headers } from "next/headers"
 
@@ -33,6 +19,6 @@ export async function getSubdomain() {
     // www.sprynt.works
     if (parts[0] === "www") return null
 
-    // subdomain.sprynt.works
+    // subdomain.sprynt.workscd 
     return parts[0]
 }
